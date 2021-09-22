facb8e01a4db122db354bd109c2d976087c07da7
Ross Brackett <ross@porkbun.com>
2021-09-22 16:33:50 -0700

    Revert "indentation problem"
    This reverts commit a8e68038554db2deadc84c459dd4c61860e9d719.

--- porkbun-ddns.py
+++ porkbun-ddns.py
@@ -52,7 +52,8 @@ if len(sys.argv)>2: #at least the config and root domain is specified
 		myIP=sys.argv[5]
 	else:
 		myIP=getMyIP() #otherwise use the detected exterior IP address
-	deleteRecord()
+
+ 	deleteRecord()
 
 	if getRecords(fqdn)["status"]=="ERROR":
 		print(createRecord()["status"])
